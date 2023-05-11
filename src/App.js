92d309d7ad15126ba01fcc0ffb9e665b4a4dc9cf
Stephanie Powell <spowell519@gmail.com>
2023-05-11 19:36:13 -0400

    New quattro application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import WelcomePage from "./Welcome";
+import "./App.css";
+import {useNavigate, Route, Routes} from 'react-router-dom';
 
-function App() {
+function App(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000", { username, password })
+      .then((response) => {
+        console.log(response);
+        navigate('/welcome');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path= "/Welcome" element={  <WelcomePage {...props} username={username} />} />
+        <Route path= "/" element={<h1>Welcome to the Game </h1>} />
+      </Routes>
+    
+      <form onSubmit={handleSubmit} action="/welcome">
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="text" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
